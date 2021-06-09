06276c264a8b85f6ed1bb0c9ddfa5474dea78269
Siddartha Thentu <siddarthathentu@gmail.com>
2021-06-08 19:09:38 -0700

    Updated Prc File

---
+++ app/run.py
@@ -0,0 +1,168 @@
+import json
+import plotly
+import pandas as pd
+
+import nltk
+nltk.download('stopwords')
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from sklearn.base import BaseEstimator, TransformerMixin
+
+from flask import Flask
+from flask import render_template, request, jsonify
+from plotly.graph_objs import Bar
+#from sklearn.externals import joblib
+import joblib
+from sqlalchemy import create_engine
+import re
+
+
+app = Flask(__name__)
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(sentence.split())
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+
+# class Whatever(BaseEstimator, TransformerMixin):
+#     def __init__(self):
+#         pass
+
+#     def fit(self, X, y=None):
+#         return self
+
+#     def transform(self, X):
+
+def tokenize(text):
+
+            # Define url pattern
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+
+            # Detect and replace urls
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+            # tokenize sentences
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+            # save cleaned tokens
+    clean_tokens = [lemmatizer.lemmatize(tok).lower().strip() for tok in tokens]
+
+            # remove stopwords
+    STOPWORDS = list(set(stopwords.words('english')))
+    clean_tokens = [token for token in clean_tokens if token not in STOPWORDS]
+
+            #return ' '.join(clean_tokens)
+    return clean_tokens
+    #return pd.Series(X).apply(tokenize).values
+
+# load data
+engine = create_engine('sqlite:///data/DisasterResponse.db')
+df = pd.read_sql_table('DisasterResponse', engine)
+
+# load model
+model = joblib.load("models/classifier.pkl")
+
+
+# index webpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
+def index():
+    
+    # extract data needed for visuals
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+
+    category_names = df.iloc[:,4:].columns
+    category_boolean = (df.iloc[:,4:] != 0).sum().values
+    
+    # create visuals
+    graphs = [
+        {
+            'data': [
+                Bar(
+                    x=genre_names,
+                    y=genre_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Genres',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        },
+
+       {
+            'data': [
+                Bar(
+                    x=category_names,
+                    y=category_boolean
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Categories',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Category",
+                    'tickangle': 35
+                }
+            }
+        }
+    ]
+    
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    # render web page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
+
+# web page that handles user query and displays model results
+@app.route('/go')
+def go():
+    # save user input in query
+    query = request.args.get('query', '') 
+
+    # use model to predict classification for query
+    classification_labels = model.predict([query])[0]
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # This will render the go.html Please see that file. 
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
+
+
+# def main():
+#     app.run(port=5000, debug=True)
+
+
+if __name__ == '__main__':
+    app.run()
